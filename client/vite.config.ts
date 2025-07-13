d2b4c467b5c1d26b1ae64a74505ee847dd6540e1
Munish <munishkumar631@gmail.com>
2025-07-13 22:27:18 +0530

    fix workflow

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,13 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
